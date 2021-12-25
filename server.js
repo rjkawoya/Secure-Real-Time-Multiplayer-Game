438c110aaa45a154330b59a78e56326870c2e829
R.J. Kawoya <89083293+rjkawoya@users.noreply.github.com>
2021-12-25 21:10:44 +0000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,69 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const expect = require('chai');
+const socket = require('socket.io');
+const cors = require('cors');
+const helmet = require("helmet");
+const nocache = require("nocache");
+
+const fccTestingRoutes = require('./routes/fcctesting.js');
+const runner = require('./test-runner.js');
+const app = express();
+
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/assets', express.static(process.cwd() + '/assets'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  helmet({
+    noSniff: true,
+    xssFilter: true,
+    hidePoweredBy: {
+      setTo: "PHP 7.4.3",
+    },
+  })
+);
+
+app.use(nocache());
+
+//For FCC testing purposes and enables user to connect from outside the hosting platform
+app.use(cors({origin: '*'})); 
+
+// Index page (static HTML)
+app.route('/')
+  .get(function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+  }); 
+
+//For FCC testing purposes
+fccTestingRoutes(app);
+    
+// 404 Not Found Middleware
+app.use(function(req, res, next) {
+  res.status(404)
+    .type('text')
+    .send('Not Found');
+});
+
+const portNum = process.env.PORT || 3000;
+
+// Set up server and tests
+const server = app.listen(portNum, () => {
+  console.log(`Listening on port ${portNum}`);
+  if (process.env.NODE_ENV==='test') {
+    console.log('Running Tests...');
+    setTimeout(function () {
+      try {
+        runner.run();
+      } catch (error) {
+        console.log('Tests are not valid:');
+        console.error(error);
+      }
+    }, 1500);
+  }
+});
+
+module.exports = app; // For testing
